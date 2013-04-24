1a2564409daddef6d1ed9e1a264d58361b2d0a31
void <git.email.address@googlemail.com>
2013-04-24 19:08:12 +0200

    Nadir

---
+++ Source/Common/Properties/AssemblyInfo.template.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Shashkrid - Common")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1c549e4-d1b0-4c1e-b3ad-6d655bf52a68")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
